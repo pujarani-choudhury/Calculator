ebbc34b43cace58677a4f3da6e187d9d10b59ea3
pujarani-choudhury <pujac3010@gmail.com>
2025-09-20 13:14:06 +0530

    Updated the js

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ buttons.forEach(button => {
 
     if (value === "AC") {
       display.value = "";
-    } else if (value === "DE") {
+    } 
+    else if (value === "DE") {
       display.value = display.value.slice(0, -1);
-    } else if (value === "=") {
+    } 
+    else if (value === "=") {
       try {
         if (display.value === "") return;
         display.value = eval(display.value);
